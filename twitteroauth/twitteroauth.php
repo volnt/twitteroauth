af80b52613268d891e73f26fc847254984fbc10d
Amal Raghav <amal.raghav@gmail.com>
2011-03-15 17:44:45 +0530

    patch for better compatibility with TwitterBundle

--- twitteroauth/twitteroauth.php
+++ twitteroauth/twitteroauth.php
@@ -78,9 +78,8 @@ class TwitterOAuth {
       $parameters['oauth_callback'] = $oauth_callback;
     } 
     $request = $this->oAuthRequest($this->requestTokenURL(), 'GET', $parameters);
-    $token = OAuthUtil::parse_parameters($request);
-    $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
-    return $token;
+    
+    return $this->getToken($request);
   }
 
   /**
@@ -114,9 +113,8 @@ class TwitterOAuth {
       $parameters['oauth_verifier'] = $oauth_verifier;
     }
     $request = $this->oAuthRequest($this->accessTokenURL(), 'GET', $parameters);
-    $token = OAuthUtil::parse_parameters($request);
-    $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
-    return $token;
+
+    return $this->getToken($request);
   }
 
   /**
@@ -134,9 +132,8 @@ class TwitterOAuth {
     $parameters['x_auth_password'] = $password;
     $parameters['x_auth_mode'] = 'client_auth';
     $request = $this->oAuthRequest($this->accessTokenURL(), 'POST', $parameters);
-    $token = OAuthUtil::parse_parameters($request);
-    $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
-    return $token;
+
+    return $this->getToken($request);
   }
 
   /**
@@ -241,5 +238,29 @@ class TwitterOAuth {
       $this->http_header[$key] = $value;
     }
     return strlen($header);
+  }
+
+  /**
+   * Added to go well with the Symfony2 DIC
+   * @param  $oauth_token
+   * @param  $oauth_token_secret
+   * @return void
+   */
+  function setOAuthToken($oauth_token, $oauth_token_secret) {
+      $this->token = new OAuthConsumer($oauth_token, $oauth_token_secret);
+  }
+
+  /**
+   * Avoid the notices if the token is not set
+   * @param  $request
+   * @return array
+   */
+  function getToken($request) {
+    $token = OAuthUtil::parse_parameters($request);
+    if (isset($token['oauth_token'], $token['oauth_token_secret'])) {
+        $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
+    }
+
+    return $token;
   }
 }
